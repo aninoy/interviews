2156c7b9dfbdbc956740f79a2a818ca3ef77a2b3
Aninoy Mahapatra <amahapat@usc.edu>
2018-03-22 01:17:40 -0700

    Leetcode practice continues.

---
+++ python/unique_bst.py
@@ -0,0 +1,35 @@
+def recursive_bst(parent, rest):
+    print('Start:', parent, rest)
+    count = 0
+    if len(rest) <= 1:
+        if parent is None:
+            count = 1
+        else:
+            count = 0
+    else:
+        for root in rest:
+            left_rest = [i for i in rest if i < root]
+            right_rest = [i for i in rest if i > root]
+            print(root, left_rest, right_rest)
+            lc = recursive_bst(root, left_rest)
+            rc = recursive_bst(root, right_rest)
+            if lc + rc == 0:
+                count += 1
+            else:
+                count += lc + rc
+    print('End:', parent, rest, count)
+    return count
+
+
+def numTrees(n):
+    rest = [x for x in range(1, n + 1)]
+    count = recursive_bst(None, rest)
+    return count
+
+
+def main():
+    count = numTrees(6)
+    print(count)
+
+if __name__ == '__main__':
+    main()
